a9cf37ef5538ea4890a83acaa85b3a8cb11c2916
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2026-02-10 11:18:33 -0300

    Update sem2_aula1_introd_C.c

--- sem2_aula1_introd_C.c
+++ sem2_aula1_introd_C.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
-
+ #include <stdio.h>
 
 int main() {
     int num1, num2, soma;
+    float div1, div2, divisao;
 
+    // comentario
     // lendo dois numeros do usuario
-    printf("Digite o primeiro numero: ");
+    printf("Digite o primeiro numero da soma: ");
     scanf("%d", &num1);
 
-    printf("Digite o segundo numero: ");
+    printf("Digite o segundo numero da soma: ");
     scanf("%d", &num2);
 
     // fazendo a soma
@@ -17,6 +18,21 @@ int main() {
     // mostrando o resultado
     printf("A soma de %d e %d eh: %d\n", num1, num2, soma);
 
-    return 0;
+    //adicionando divisao agora
+    printf("Digite o primeiro numero para a divisao: ");
+    scanf("%f", &div1);
 
+    printf("Digite o denominador para a divisao: ");
+    scanf("%f", &div2);
+
+    // testando if
+    if (div2 == 0) {
+        printf ("O denominador nao pode ser zero.\n");
+    } else {
+    divisao = div1 / div2;
+    printf("O resultado da divisao de %f e %f eh: %f\n", div1, div2, divisao);
+    }
+
+
+    return 0;
 }

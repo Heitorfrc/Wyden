90999d92850f3135bafa9abd422ef73ccd1d0992
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2026-02-10 10:47:52 -0300

    Add basic C program to sum two numbers

---
+++ sem2_aula1_introd_C.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main() {
+    int num1, num2, soma;
+
+    // comentario
+    // lendo dois numeros do usuario
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+
+    // fazendo a soma
+    soma = num1 + num2;
+
+    // mostrando o resultado
+    printf("A soma de %d e %d eh: %d\n", num1, num2, soma);
+
+    return 0;
+
+}
